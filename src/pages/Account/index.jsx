5f252ce8dc3f4a73629a7175049e65295ce07175
rajamuthukumaran <rajamuthukumaran@softsuave.com>
2021-01-18 17:05:02 +0530

    Revert "Merge branch 'signin-responsive' of https://github.com/yogaraja2/Game-Midas into development"
    This reverts commit 4a10202818efaf8a35ffcb1bdca75aeef3d94db4.
    
    Conflicts:
    	src/pages/DashBoard/DashHead.jsx

--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -17,13 +17,10 @@ function Account() {
       </Grid>
 
       <Grid item xs={12} md={8} className="account-screens">
-        {screen === 'signup' ? (
-          <Signup />
-        ) : screen === 'selectRole' ? (
-          <SelectRole />
-        ) : (
-          <Login />
-        )}
+
+        {screen === 'signup' ? <Signup /> :
+          screen === 'selectRole' ? <SelectRole /> : <Login />
+        }
 
         <div className="copyright-wrap">
           <Copyright />
--- src/pages/DashBoard/CashFlow/index.jsx
+++ src/pages/DashBoard/CashFlow/index.jsx
@@ -1,46 +1,15 @@
-import React, { useState } from 'react'
-import { Button, Grid } from '@material-ui/core'
-import Card from '../../../components/Card'
-import YearBar from '../../../components/YearBar'
+import { Grid } from '@material-ui/core'
+import React from 'react'
 import './styles.scss'
-import Income from './Income'
-import StatTotal from './StatTotal'
-import FExpenses from './FExpenses'
-import VExpenses from './VExpenses'
-import SatisfactionCard from './SatisfactionCard'
 
 function CashFlow() {
-  const [dataYear, setDataYear] = useState(1)
-  const statSectionSize = {
-    md: 4,
-    sm: 6,
-    xs: 12
-  }
-
-  return (
-    <div className="dash-cash-flow-page">
-      <YearBar value={dataYear} onClick={setDataYear} years={10} />
-
-      <div className="stat-card-wrap">
-        <h2 className="sec-head stats">Year {dataYear}</h2>
-        <Card className="stat-card">
-          <Grid container className="stat-grid-wrap">
-            <Income size={statSectionSize} />
-            <FExpenses size={statSectionSize} />
-            <VExpenses size={statSectionSize} />
-            <StatTotal size={statSectionSize} />
-          </Grid>
-        </Card>
-
-        <h3 className="sec-head sat">Satisfaction Source</h3>
-        <SatisfactionCard />
-
-        <div className="nxt-btn-wrap">
-          <Button className="nxt-btn">Next</Button>
-        </div>
-      </div>
-    </div>
-  )
+    return (
+        <Grid item xs={12} md={10} className="cash-flow-card">
+            <img src={require('../../../assets/img/TitleLogo.svg').default} alt="title-logo"
+                style={{ width: 150, height: 70 }} />
+            <h1 style={{ textAlign: 'center' }}>Cash Flow Page</h1>
+        </Grid>
+    )
 }
 
 export default CashFlow
