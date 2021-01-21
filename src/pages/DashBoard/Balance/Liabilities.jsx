e45f3bbec1b881c3135d5dde7be5c67ad16e9562
rajamuthukumaran <rajamuthukumaran@softsuave.com>
2021-01-21 22:41:20 +0530

    Remove payment option in balance

--- src/pages/DashBoard/Balance/Liabilities.jsx
+++ src/pages/DashBoard/Balance/Liabilities.jsx
@@ -22,11 +22,11 @@ const Entry = ({ label, title, balance, payment }) => {
           <GameCoin className="balance" value={balance} color="#5f5f5f" />
         </div>
       </td>
-      <td className="col col-center">
+      {/* <td className="col col-center">
         <div className="pymnt-wrap cell">
           <GameCoin className="payment" value={payment} color="#5f5f5f" />
         </div>
-      </td>
+      </td> */}
     </tr>
   )
 }
@@ -44,9 +44,9 @@ function Liabilities({ data }) {
               <th>
                 <div className="head">Balance</div>
               </th>
-              <th>
+              {/* <th>
                 <div className="head">Payment</div>
-              </th>
+              </th> */}
             </tr>
           </thead>
 
@@ -55,25 +55,25 @@ function Liabilities({ data }) {
               label="Credit Card"
               title="This is credit card"
               balance={data?.creditLoan?.balance}
-              payment={300}
+              // payment={300}
             />
             <Entry
               label="Vehicle Loan"
               title="This is credit card"
               balance={data?.carLoan?.balance}
-              payment={300}
+              // payment={300}
             />
             <Entry
               label="Mortgage"
               title="This is credit card"
               balance={data?.mortgageLoan?.balance}
-              payment={300}
+              // payment={300}
             />
             <Entry
               label="Student Loan"
               title="This is credit card"
               balance={data?.studenLoan?.balance}
-              payment={300}
+              // payment={300}
             />
           </tbody>
 
@@ -81,7 +81,7 @@ function Liabilities({ data }) {
             <Entry
               label="Total Assets"
               balance={data?.totalLiabilities}
-              payment={300}
+              // payment={300}
             />
           </tfoot>
         </table>
