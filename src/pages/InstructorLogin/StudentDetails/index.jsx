f18b86b57c92fde0e9ce3fd1643416696d4cf449
yogaraja <yogaraja.subbiah@softsuave.com>
2021-02-17 23:21:13 +0530

    Added students stats component.

--- src/pages/InstructorLogin/StudentDetails/index.jsx
+++ src/pages/InstructorLogin/StudentDetails/index.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import './style.scss'
 import { Grid } from '@material-ui/core'
 import { useSelector } from 'react-redux'
+import {
+    AccumulationChartComponent,
+    AccumulationSeriesCollectionDirective,
+    AccumulationSeriesDirective,
+    PieSeries, Inject,
+    AccumulationDataLabel
+} from '@syncfusion/ej2-react-charts'
+import { ProgressBarComponent } from '@syncfusion/ej2-react-progressbar'
 
 function StudentDetails() {
+
     const studentDetail = useSelector(state => state?.studentDetail)
+
+    const data = [
+        { x: "income", y: studentDetail.income, color: '#5EAAA8' },
+        { x: "networth", y: studentDetail.networth, color: '#FFD66B' },
+        { x: "score", y: studentDetail.totalScore, color: '#DB6400' }
+    ]
+
+    const incomePer = studentDetail.income / 150000 * 100;
+    const networthPer = studentDetail.networth / studentDetail.income * 100;
+    const scorePer = studentDetail.totalScore / studentDetail.income * 100;
+
     return (
         <Grid item xs={12} md={10} className="student-details-card">
             <div className="title">
@@ -36,8 +56,57 @@ function StudentDetails() {
                     </div>
                 </div>
                 <div className="charts-wrap">
-
+                    <AccumulationChartComponent id="charts" height="300" center={{ x: "50%", y: "50%" }}>
+                        <Inject services={[PieSeries]} />
+                        <AccumulationSeriesCollectionDirective>
+                            <AccumulationSeriesDirective
+                                dataSource={data}
+                                xName="x"
+                                yName="y"
+                                type="Pie"
+                                radius="70%"
+                                pointColorMapping="color"
+                            />
+                        </AccumulationSeriesCollectionDirective>
+                    </AccumulationChartComponent>
+                    <div className="income-field">
+                        <div className="side-head">Income</div>
+                        <ProgressBarComponent
+                            id="income"
+                            type="Linear"
+                            width="50%"
+                            value={incomePer}
+                            progressColor="#5EAAA8"
+                            animation={{ enable: true, duration: 2000, delay: 0 }}
+                        />
+                        <div className="value">{studentDetail.income}</div>
+                    </div>
+                    <div className="networth-field">
+                        <div className="side-head">Networth</div>
+                        <ProgressBarComponent
+                            id="networth"
+                            type="Linear"
+                            width="100%"
+                            value={networthPer}
+                            progressColor="#FFD66B"
+                            animation={{ enable: true, duration: 2000, delay: 0 }}
+                        />
+                        <div className="value">{studentDetail.networth}</div>
+                    </div>
+                    <div className="score-field">
+                        <div className="side-head">Score</div>
+                        <ProgressBarComponent
+                            id="score"
+                            type="Linear"
+                            width="50%"
+                            value={scorePer}
+                            progressColor="#DB6400"
+                            animation={{ enable: true, duration: 2000, delay: 0 }}
+                        />
+                        <div className="value">{studentDetail.totalScore}</div>
+                    </div>
                 </div>
+                
                 <div className="avatar-wrap">
                     <img src={require(`../../../assets/img/Avatar${studentDetail.avatarIcon}.svg`).default} alt="user-avatar" />
                 </div>
--- src/pages/SchoolAdminLogin/StudentDetails/index.jsx
+++ src/pages/SchoolAdminLogin/StudentDetails/index.jsx
@@ -1,26 +1,3 @@
-import React from 'react'
-import { Grid } from '@material-ui/core'
-import { useSelector } from 'react-redux'
+import StudentDetails from '../../InstructorLogin/StudentDetails'
 
-function StudentDetails() {
-    const studentDetail = useSelector(state => state.studentDetail)
-    return (
-        <Grid item xs={12} md={10} className="student-details-card">
-            <div className="title">
-                Student Stats
-            </div>
-            <div className="stu-detail-wrap">
-                <div>Name : {studentDetail.name}</div>
-                <div>Rank : {studentDetail.leaderBoardRank}</div>
-                <div>Score : {studentDetail.totalScore}</div>
-                <div>Income : {studentDetail.income || 0}</div>
-                <div>Networth : {studentDetail.networth || 0}</div>
-                <div>Game Mode : {studentDetail.role || 0}</div>
-                <div>Game Length : {studentDetail.gameLength || 0}</div>
-                <div>Country : {studentDetail.country}</div>
-            </div>
-        </Grid>
-    )
-}
-
-export default StudentDetails
+export default StudentDetails;
